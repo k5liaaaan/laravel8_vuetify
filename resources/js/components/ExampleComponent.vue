9ddee1d6b6f6e348b38fa75282753da017ebcfbd
k-harada <k-harada@hheart.jp>
2021-05-20 22:55:29 +0900

    second commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-app>
+        <header>
+            <v-app-bar
+                app
+                dark
+            >
+                <v-toolbar-title>Laravel Vuetfy</v-toolbar-title>
+            </v-app-bar>
+        </header>
+    </v-app>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
